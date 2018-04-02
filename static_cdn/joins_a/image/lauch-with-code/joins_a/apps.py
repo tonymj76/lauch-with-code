111d4e9a5f0999e1127c58c615b31198a887b2d2
Anthony <demeyortj@gmail.com>
2018-04-02 10:09:10 +0100

    modified:   Pipfile         modified:   Pipfile.lock         modified:   joins_a/migrations/0001_initial.py         deleted:    joins_a/migrations/0002_auto_20180321_1305.py         deleted:    joins_a/migrations/0003_joinfriends.py         deleted:    joins_a/migrations/0004_auto_20180324_1055.py         deleted:    joins_a/migrations/0005_auto_20180324_1126.py         deleted:    joins_a/migrations/0006_auto_20180401_1610.py         modified:   joins_a/models.py         modified:   requirments.txt         modified:   subscribe_p2/settings/__init__.py         modified:   subscribe_p2/settings/base.py         modified:   subscribe_p2/settings/local.py         modified:   subscribe_p2/settings/production.py  static_root/

--- static_cdn/joins_a/image/lauch-with-code/joins_a/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoinsAConfig(AppConfig):
-    name = 'joins_a'
--- static_cdn/joins_a/image/lauch-with-code/joins_a/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Join
-
-class JoinForm(forms.ModelForm):
-    """ the Join form to be displayed when registration """
-    class Meta:
-        model = Join
-        fields = ("first_name", "email_address")
